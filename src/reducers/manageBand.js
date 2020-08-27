5de71c96b5893c1ae12ff41d864ac1ac0e5983af
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-27 20:54:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -6,8 +6,14 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+    const band = {
+      id: uuid(),
+      name: action.name
+    }
+    return { bands: state.bands.concat(band) };
 
-      return { ...state, bands: [...state.bands, action.name] }
+    case 'DELETE_BAND':
+      return {bands: state.bands.filter(band => band.id !== action.id)}
 
     default:
       return state;
