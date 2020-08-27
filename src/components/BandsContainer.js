15ff8ba18423291f2ff7b2860a08ee800a497479
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-27 20:52:39 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import BandInput from './BandInput';
 
 import { connect } from 'react-redux'
+import Bands from './Bands';
 
 class BandsContainer extends Component {
   render() {
